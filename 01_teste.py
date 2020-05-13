7a5f4b0aef2ecb15b70eaa8812f47b80af201352
Alef Figueiredo <alef_figueiredo@hotmail.com>
2020-05-13 08:52:34 -0300

    adicionei 01_teste e 02_teste

---
+++ 01_teste.py
@@ -0,0 +1,6 @@
+print('=' * 5, 'TESTE-01', '=' * 5)
+nome = input('Qual o seu nome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual seu peso? ')
+print('Olá {}! Você está aprendendo a programar aos {} anos e tem {}kg.'.format(nome, idade, peso))
+print('Vamos ver o quanto vai engordar. kkk')
